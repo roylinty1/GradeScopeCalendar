a1a3c8abcc87a1dc4812e0324ad6b2a706619d55
planit <planit@tanjeffreyz.github.io>
2023-10-13 06:37:20 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -183,14 +183,14 @@ assignments = {
       "title": "Assignment 1, Deliverable 3 (report+pdf of your code)",
       "course": "INDENG 174",
       "dueDate": "2023-10-12T23:00:00",
-      "link": "https://www.gradescope.com/courses/585118/assignments/3470275/submissions/201805894",
+      "link": "https://www.gradescope.com/courses/585118/assignments/3470275/submissions/202208213",
       "submitted": true
     },
     {
       "title": "Assignment 1, Deliverable 3 (.ipynb code)",
       "course": "INDENG 174",
       "dueDate": "2023-10-12T23:00:00",
-      "link": "https://www.gradescope.com/courses/585118/assignments/3470293/submissions/201805997",
+      "link": "https://www.gradescope.com/courses/585118/assignments/3470293/submissions/202208379",
       "submitted": true
     }
   ]
