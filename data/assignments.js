dbf8375e731d537e5964ee6b488df7da1d0fc22d
planit <planit@tanjeffreyz.github.io>
2023-11-18 09:34:46 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -176,6 +176,13 @@ assignments = {
       "dueDate": "2023-11-18T23:59:00",
       "link": "https://www.gradescope.com/courses/563079/assignments/3673184/submissions/212755273",
       "submitted": true
+    },
+    {
+      "title": "PS 11&12",
+      "course": "IEOR 166",
+      "dueDate": "2023-11-30T09:00:00",
+      "link": "https://www.gradescope.com/courses/563079",
+      "submitted": false
     }
   ],
   "INDENG 174": [
