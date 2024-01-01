89163f0c5bd3b44d2d21cbbcf6d263f095c18fa2
planit <planit@tanjeffreyz.github.io>
2024-01-01 01:43:46 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -3,105 +3,105 @@ assignments = {
     {
       "title": "Lab 1",
       "course": "DATA 88E",
-      "dueDate": "2023-09-05T23:59:00",
+      "dueDate": "2024-09-05T23:59:00",
       "link": "https://www.gradescope.com/courses/573841/assignments/3219307/submissions/190481622",
       "submitted": true
     },
     {
       "title": "Project 1",
       "course": "DATA 88E",
-      "dueDate": "2023-09-19T23:59:00",
+      "dueDate": "2024-09-19T23:59:00",
       "link": "https://www.gradescope.com/courses/573841/assignments/3219308/submissions/194280174",
       "submitted": true
     },
     {
       "title": "Lab 2",
       "course": "DATA 88E",
-      "dueDate": "2023-09-12T23:59:00",
+      "dueDate": "2024-09-12T23:59:00",
       "link": "https://www.gradescope.com/courses/573841/assignments/3272278/submissions/192212468",
       "submitted": true
     },
     {
       "title": "Lab 3",
       "course": "DATA 88E",
-      "dueDate": "2023-09-19T23:59:00",
+      "dueDate": "2024-09-19T23:59:00",
       "link": "https://www.gradescope.com/courses/573841/assignments/3328022/submissions/194261296",
       "submitted": true
     },
     {
       "title": "Lab 4",
       "course": "DATA 88E",
-      "dueDate": "2023-09-26T23:59:00",
+      "dueDate": "2024-09-26T23:59:00",
       "link": "https://www.gradescope.com/courses/573841/assignments/3376528/submissions/196983695",
       "submitted": true
     },
     {
       "title": "Project 2",
       "course": "DATA 88E",
-      "dueDate": "2023-10-10T23:59:00",
+      "dueDate": "2024-10-10T23:59:00",
       "link": "https://www.gradescope.com/courses/573841/assignments/3376529/submissions/200979229",
       "submitted": true
     },
     {
       "title": "Lab 5",
       "course": "DATA 88E",
-      "dueDate": "2023-10-04T23:59:00",
+      "dueDate": "2024-10-04T23:59:00",
       "link": "https://www.gradescope.com/courses/573841/assignments/3425353/submissions/198739037",
       "submitted": true
     },
     {
       "title": "Lab 6",
       "course": "DATA 88E",
-      "dueDate": "2023-10-10T23:59:00",
+      "dueDate": "2024-10-10T23:59:00",
       "link": "https://www.gradescope.com/courses/573841/assignments/3472866/submissions/200359512",
       "submitted": true
     },
     {
       "title": "Project 3",
       "course": "DATA 88E",
-      "dueDate": "2023-10-31T23:59:00",
+      "dueDate": "2024-10-31T23:59:00",
       "link": "https://www.gradescope.com/courses/573841/assignments/3511726/submissions/207922632",
       "submitted": true
     },
     {
       "title": "Lab 7",
       "course": "DATA 88E",
-      "dueDate": "2023-10-17T23:59:00",
+      "dueDate": "2024-10-17T23:59:00",
       "link": "https://www.gradescope.com/courses/573841/assignments/3511729/submissions/203221280",
       "submitted": true
     },
     {
       "title": "Lab 8",
       "course": "DATA 88E",
-      "dueDate": "2023-10-24T23:59:00",
+      "dueDate": "2024-10-24T23:59:00",
       "link": "https://www.gradescope.com/courses/573841/assignments/3551152/submissions/205397000",
       "submitted": true
     },
     {
       "title": "Lab 9",
       "course": "DATA 88E",
-      "dueDate": "2023-10-31T23:59:00",
+      "dueDate": "2024-10-31T23:59:00",
       "link": "https://www.gradescope.com/courses/573841/assignments/3588945/submissions/206770371",
       "submitted": true
     },
     {
       "title": "Project 4",
       "course": "DATA 88E",
-      "dueDate": "2023-11-28T23:59:00",
+      "dueDate": "2024-11-28T23:59:00",
       "link": "https://www.gradescope.com/courses/573841/assignments/3620012/submissions/213931152",
       "submitted": true
     },
     {
       "title": "Lab 10",
       "course": "DATA 88E",
-      "dueDate": "2023-11-14T23:59:00",
+      "dueDate": "2024-11-14T23:59:00",
       "link": "https://www.gradescope.com/courses/573841/assignments/3657192/submissions/211696365",
       "submitted": true
     },
     {
       "title": "Lab 11",
       "course": "DATA 88E",
-      "dueDate": "2023-12-06T23:59:00",
+      "dueDate": "2024-12-06T23:59:00",
       "link": "https://www.gradescope.com/courses/573841/assignments/3733483/submissions/216015150",
       "submitted": true
     }
@@ -110,98 +110,98 @@ assignments = {
     {
       "title": "PS 1",
       "course": "IEOR 166",
-      "dueDate": "2023-08-31T09:00:00",
+      "dueDate": "2024-08-31T09:00:00",
       "link": "https://www.gradescope.com/courses/563079/assignments/3193188/submissions/188713746",
       "submitted": true
     },
     {
       "title": "PS 2",
       "course": "IEOR 166",
-      "dueDate": "2023-09-07T09:00:00",
+      "dueDate": "2024-09-07T09:00:00",
       "link": "https://www.gradescope.com/courses/563079/assignments/3248903/submissions/190446431",
       "submitted": true
     },
     {
       "title": "Assignment 3B",
       "course": "IEOR 166",
-      "dueDate": "2023-09-19T23:59:00",
+      "dueDate": "2024-09-19T23:59:00",
       "link": "https://www.gradescope.com/courses/563079/assignments/3254945/submissions/193841579",
       "submitted": true
     },
     {
       "title": "Assignment 3A",
       "course": "IEOR 166",
-      "dueDate": "2023-09-14T09:00:00",
+      "dueDate": "2024-09-14T09:00:00",
       "link": "https://www.gradescope.com/courses/563079/assignments/3301562/submissions/192320832",
       "submitted": true
     },
     {
       "title": "Optional: Project proposal",
       "course": "IEOR 166",
-      "dueDate": "2023-09-22T14:03:00",
+      "dueDate": "2024-09-22T14:03:00",
       "link": "https://www.gradescope.com/courses/563079",
       "submitted": false
     },
     {
       "title": "PS 5",
       "course": "IEOR 166",
-      "dueDate": "2023-10-05T09:00:00",
+      "dueDate": "2024-10-05T09:00:00",
       "link": "https://www.gradescope.com/courses/563079/assignments/3462048/submissions/199415668",
       "submitted": true
     },
     {
       "title": "PS 6",
       "course": "IEOR 166",
-      "dueDate": "2023-10-12T21:00:00",
+      "dueDate": "2024-10-12T21:00:00",
       "link": "https://www.gradescope.com/courses/563079/assignments/3487139/submissions/201754339",
       "submitted": true
     },
     {
       "title": "PS 7A",
       "course": "IEOR 166",
-      "dueDate": "2023-10-19T21:00:00",
+      "dueDate": "2024-10-19T21:00:00",
       "link": "https://www.gradescope.com/courses/563079/assignments/3526471/submissions/203552027",
       "submitted": true
     },
     {
       "title": "Assignment 7B",
       "course": "IEOR 166",
-      "dueDate": "2023-10-24T23:59:00",
+      "dueDate": "2024-10-24T23:59:00",
       "link": "https://www.gradescope.com/courses/563079/assignments/3526480/submissions/204988241",
       "submitted": true
     },
     {
       "title": "PS 9",
       "course": "IEOR 166",
-      "dueDate": "2023-11-12T23:59:00",
+      "dueDate": "2024-11-12T23:59:00",
       "link": "https://www.gradescope.com/courses/563079/assignments/3616472/submissions/211316289",
       "submitted": true
     },
     {
       "title": "PS 10",
       "course": "IEOR 166",
-      "dueDate": "2023-11-18T23:59:00",
+      "dueDate": "2024-11-18T23:59:00",
       "link": "https://www.gradescope.com/courses/563079/assignments/3673184/submissions/212755273",
       "submitted": true
     },
     {
       "title": "PS 11&12",
       "course": "IEOR 166",
-      "dueDate": "2023-11-30T23:59:00",
+      "dueDate": "2024-11-30T23:59:00",
       "link": "https://www.gradescope.com/courses/563079/assignments/3702188/submissions/216250102",
       "submitted": true
     },
     {
       "title": "PS 13",
       "course": "IEOR 166",
-      "dueDate": "2023-12-08T23:59:00",
+      "dueDate": "2024-12-08T23:59:00",
       "link": "https://www.gradescope.com/courses/563079/assignments/3705470/submissions/218374591",
       "submitted": true
     },
     {
       "title": "PS 14",
       "course": "IEOR 166",
-      "dueDate": "2023-12-11T23:59:00",
+      "dueDate": "2024-12-11T23:59:00",
       "link": "https://www.gradescope.com/courses/563079/assignments/3705472/submissions/218374886",
       "submitted": true
     }
@@ -210,84 +210,84 @@ assignments = {
     {
       "title": "Assignment 1, Deliverable 1 (report+pdf of your code)",
       "course": "INDENG 174",
-      "dueDate": "2023-09-14T23:00:00",
+      "dueDate": "2024-09-14T23:00:00",
       "link": "https://www.gradescope.com/courses/585118/assignments/3242317/submissions/192418590",
       "submitted": true
     },
     {
       "title": "Assignment 1, Deliverable 1 (.ipynb code)",
       "course": "INDENG 174",
-      "dueDate": "2023-09-14T23:00:00",
+      "dueDate": "2024-09-14T23:00:00",
       "link": "https://www.gradescope.com/courses/585118/assignments/3255784/submissions/192015767",
       "submitted": true
     },
     {
       "title": "Project Proposal",
       "course": "INDENG 174",
-      "dueDate": "2023-10-05T23:00:00",
+      "dueDate": "2024-10-05T23:00:00",
       "link": "https://www.gradescope.com/courses/585118/assignments/3329833/submissions/199658946",
       "submitted": true
     },
     {
       "title": "Project Progress report",
       "course": "INDENG 174",
-      "dueDate": "2023-11-02T23:00:00",
+      "dueDate": "2024-11-02T23:00:00",
       "link": "https://www.gradescope.com/courses/585118/assignments/3329858/submissions/209047586",
       "submitted": true
     },
     {
       "title": "Project Draft Report",
       "course": "INDENG 174",
-      "dueDate": "2023-11-27T23:00:00",
+      "dueDate": "2024-11-27T23:00:00",
       "link": "https://www.gradescope.com/courses/585118/assignments/3329862/submissions/215533757",
       "submitted": true
     },
     {
       "title": "Project Final Report",
       "course": "INDENG 174",
-      "dueDate": "2023-12-16T23:00:00",
+      "dueDate": "2024-12-16T23:00:00",
       "link": "https://www.gradescope.com/courses/585118/assignments/3329863/submissions/220380086",
       "submitted": true
     },
     {
       "title": "Assignment 1, Deliverable 2 (report+pdf of your code)",
       "course": "INDENG 174",
-      "dueDate": "2023-09-28T23:00:00",
+      "dueDate": "2024-09-28T23:00:00",
       "link": "https://www.gradescope.com/courses/585118/assignments/3352675/submissions/197297486",
       "submitted": true
     },
     {
       "title": "Assignment 1, Deliverable 2 (.ipynb code)",
       "course": "INDENG 174",
-      "dueDate": "2023-09-28T23:00:00",
+      "dueDate": "2024-09-28T23:00:00",
       "link": "https://www.gradescope.com/courses/585118/assignments/3352681/submissions/197297816",
       "submitted": true
     },
     {
       "title": "Assignment 1, Deliverable 3 (report+pdf of your code)",
       "course": "INDENG 174",
-      "dueDate": "2023-10-12T23:00:00",
+      "dueDate": "2024-10-12T23:00:00",
       "link": "https://www.gradescope.com/courses/585118/assignments/3470275/submissions/202208213",
       "submitted": true
     },
     {
       "title": "Assignment 1, Deliverable 3 (.ipynb code)",
       "course": "INDENG 174",
-      "dueDate": "2023-10-12T23:00:00",
+      "dueDate": "2024-10-12T23:00:00",
       "link": "https://www.gradescope.com/courses/585118/assignments/3470293/submissions/202208379",
       "submitted": true
     },
     {
       "title": "Assignment 2 (report)",
       "course": "INDENG 174",
-      "dueDate": "2023-11-09T23:00:00",
+      "dueDate": "2024-11-09T23:00:00",
       "link": "https://www.gradescope.com/courses/585118/assignments/3606619/submissions/211233021",
       "submitted": true
     },
     {
       "title": "Assignment 2 (simio file)",
       "course": "INDENG 174",
-      "dueDate": "2023-11-09T23:00:00",
+      "dueDate": "2024-11-09T23:00:00",
       "link": "https://www.gradescope.com/courses/585118/assignments/3606633/submissions/209704188",
       "submitted": true
     }
