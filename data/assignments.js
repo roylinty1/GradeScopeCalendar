2c77dac39eacf73753018ac6628c58cc33b5e533
planit <planit@tanjeffreyz.github.io>
2024-01-25 01:40:33 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -1,4 +1,12 @@
 assignments = {
   "IEOR 156": [],
-  "INDENG 164": []
+  "INDENG 164": [
+    {
+      "title": "Assignment 1",
+      "course": "INDENG 164",
+      "dueDate": "2024-01-31T23:00:00",
+      "link": "https://www.gradescope.com/courses/693508",
+      "submitted": false
+    }
+  ]
 };
