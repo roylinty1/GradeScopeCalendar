bcb04591d9c4b0911534c9933128a8b8a2b39686
planit <planit@tanjeffreyz.github.io>
2024-01-31 23:34:57 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -15,6 +15,13 @@ assignments = {
       "dueDate": "2024-01-31T23:00:00",
       "link": "https://www.gradescope.com/courses/693508/assignments/3998523/submissions/226202950",
       "submitted": true
+    },
+    {
+      "title": "Assignment 2",
+      "course": "INDENG 164",
+      "dueDate": "2024-02-07T23:00:00",
+      "link": "https://www.gradescope.com/courses/693508",
+      "submitted": false
     }
   ]
 };
