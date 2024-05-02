cd6faf2add52ecaaf16df21014a7754d07d453d9
planit <planit@tanjeffreyz.github.io>
2024-05-02 06:36:09 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -53,8 +53,8 @@ assignments = {
       "title": "Final Case Report",
       "course": "IEOR 156",
       "dueDate": "2024-05-06T23:59:00",
-      "link": "https://www.gradescope.com/courses/719070",
-      "submitted": false
+      "link": "https://www.gradescope.com/courses/719070/assignments/4232828/submissions/251685397",
+      "submitted": true
     },
     {
       "title": "Midterm",
