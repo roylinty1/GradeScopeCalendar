898414bb90ac2319e7ae0e2a222178a523e68277
planit <planit@tanjeffreyz.github.io>
2024-09-02 21:35:30 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -1,3 +1,11 @@
 assignments = {
-  "IEOR 263A": []
+  "IEOR 263A": [
+    {
+      "title": "Problem Set 1",
+      "course": "IEOR 263A",
+      "dueDate": "2024-09-05T12:30:00",
+      "link": "https://www.gradescope.com/courses/824022",
+      "submitted": false
+    }
+  ]
 };
