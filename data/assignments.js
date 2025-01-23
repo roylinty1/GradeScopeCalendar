d3b3f9b2f3e1d5d3a85c0f0fece3f144c118f2f9
planit <planit@tanjeffreyz.github.io>
2025-01-23 02:56:52 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -1,3 +1,11 @@
 assignments = {
-  "INDENG 290-LEC-003": []
+  "INDENG 290-LEC-003": [
+    {
+      "title": "HW 1",
+      "course": "INDENG 290-LEC-003",
+      "dueDate": "2025-02-10T10:00:00",
+      "link": "https://www.gradescope.com/courses/961516",
+      "submitted": false
+    }
+  ]
 };
