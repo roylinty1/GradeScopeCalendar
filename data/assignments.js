52f9fb8a84a6ec2dad6cc8cda761595fb6d4efbf
planit <planit@tanjeffreyz.github.io>
2025-01-24 01:55:06 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -1,5 +1,13 @@
 assignments = {
-  "IEOR 267": [],
+  "IEOR 267": [
+    {
+      "title": "Problem Set 1",
+      "course": "IEOR 267",
+      "dueDate": "2025-01-30T11:00:00",
+      "link": "https://www.gradescope.com/courses/965785",
+      "submitted": false
+    }
+  ],
   "INDENG 290-LEC-003": [
     {
       "title": "HW 1",
