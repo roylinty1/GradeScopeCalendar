5b2364eea067d442e1de63c9715071948a6b680b
planit <planit@tanjeffreyz.github.io>
2025-03-07 20:36:13 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -78,8 +78,8 @@ assignments = {
       "title": "HW 2",
       "course": "INDENG 290-LEC-003",
       "dueDate": "2025-03-10T10:00:00",
-      "link": "https://www.gradescope.com/courses/961516",
-      "submitted": false
+      "link": "https://www.gradescope.com/courses/961516/assignments/5818790/submissions/315403905",
+      "submitted": true
     }
   ]
 };
